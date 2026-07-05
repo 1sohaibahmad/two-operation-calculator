5ad3e2f46fb38be8034e4fc36b0e322eb524cbe1
Suhaib Ahmad <suhaibahmad507@mail.com>
2026-07-06 00:55:40 +0500

    Add Clear Button and Make Professional UI

--- function/script.js
+++ function/script.js
@@ -1,12 +1,10 @@
-// alert("Hello")
-
 function sum_value() {
   let first_value = document.getElementById("first-value");
   let f = first_value.value;
   let second_value = document.getElementById("second-value");
   let s = second_value.value;
   let sum = Number(f) + Number(s);
-  document.getElementById('sum-of-value').innerHTML = "The Sum is " + sum;
+  document.getElementById("sum-of-value").innerHTML = "The Sum is " + sum;
 }
 
 function sub_value() {
@@ -15,6 +13,11 @@ function sub_value() {
   let second_value = document.getElementById("second-value");
   let s = second_value.value;
   let sum = Number(f) - Number(s);
-//   alert("The Difference is " + sum)
-  document.getElementById('sum-of-value').innerHTML = "The Difference is " + sum;
-}n
+  document.getElementById("sum-of-value").innerHTML = "The Difference is " + sum;
+}
+
+function clear_values() {
+  document.getElementById("first-value").value = "";
+  document.getElementById("second-value").value = "";
+  document.getElementById("sum-of-value").innerHTML = "Your result will appear here.";
+}
